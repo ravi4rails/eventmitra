0cbd5635ae3cbefa2468e9a91c6d5ccb38a29b39
Ravi Singh <ravionrails@gmail.com>
2019-07-31 18:46:45 +0530

    added home page and header links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions', 
     registrations: 'users/registrations', 
     passwords: 'users/passwords', 
     confirmations: 'users/confirmations', 
-    unlocks: 'users/unlocks', 
+    unlocks: 'users/unlocks',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
 end
